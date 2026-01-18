2e58f20fc34d4f50b47509e220b7aaa6dfc8615c
Mickael <mickael.mesloub@gmail.com>
2026-01-18 17:10:45 +0100

    2.3 : Réagissez aux événements

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -4,21 +4,45 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-face-snap',
   imports: [],
   templateUrl: './face-snap.component.html',
-  styleUrl: './face-snap.component.scss'
+  styleUrl: './face-snap.component.scss',
 })
-
 export class FaceSnapComponent implements OnInit {
   title!: string;
   description!: string;
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
-    this.title = "Archibald";
-    this.description = "Mon meilleur ami depuis toujours !";
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
     this.createdAt = new Date();
     this.snaps = 0;
-    this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  onToggleSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+
+    this.userHasSnapped = !this.userHasSnapped;
+  }
+
+  snap(): void {
+    this.snaps++;
+    this.snapButtonText = 'Oops,  unsnap!';
+  }
+
+  unSnap(): void {
+    this.snaps--;
+    this.snapButtonText = 'Oh Snap!';
   }
 }
