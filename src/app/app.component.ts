d00cf048e5a43feb1651105796104680badf98b1
Mickael <mickael.mesloub@gmail.com>
2026-01-18 17:39:38 +0100

    2.4 : Ajoutez des propriétés personnalisées

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    FaceSnapComponent
-  ],
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
+export class AppComponent implements OnInit {
+  bulbizarreSnap!: FaceSnap;
+  carapuceSnap!: FaceSnap;
+  salamecheSnap!: FaceSnap;
 
-export class AppComponent {}
- 
+  ngOnInit(): void {
+    this.bulbizarreSnap = new FaceSnap(
+      'Bulbizarre',
+      'Pokémon plante',
+      '/snaps/bulbizarre.png',
+      new Date(),
+      1,
+    );
+
+    this.carapuceSnap = new FaceSnap(
+      'Carapuce',
+      'Pokémon eau',
+      '/snaps/carapuce.png',
+      new Date(),
+      2,
+    );
+
+    this.salamecheSnap = new FaceSnap(
+      'Salamèche',
+      'Pokémon feu',
+      '/snaps/salameche.png',
+      new Date(),
+      3,
+    );
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,21 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss',
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
   snapButtonText!: string;
   userHasSnapped!: boolean;
 
   ngOnInit(): void {
-    this.title = 'Archibald';
-    this.description = 'Mon meilleur ami depuis toujours !';
-    this.createdAt = new Date();
-    this.snaps = 0;
-    this.imageUrl =
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
     this.snapButtonText = 'Oh Snap!';
     this.userHasSnapped = false;
   }
@@ -37,12 +28,12 @@ export class FaceSnapComponent implements OnInit {
   }
 
   snap(): void {
-    this.snaps++;
+    this.faceSnap.addSnap();
     this.snapButtonText = 'Oops, unsnap!';
   }
 
   unSnap(): void {
-    this.snaps--;
+    this.faceSnap.removeSnap();
     this.snapButtonText = 'Oh Snap!';
   }
 }
