2c81e28dd5e031e3ebe0a1975c0871ec9a8e3a57
Mickael <mickael.mesloub@gmail.com>
2026-04-18 22:16:43 +0200

    4.2 : Formatez les dates

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgClass, NgStyle, TitleCasePipe } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
-  imports: [NgStyle, NgClass, TitleCasePipe],
+  imports: [NgStyle, NgClass, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
